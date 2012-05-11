22a60906dee3bc622d6edce1fca49f463ba6d79b
Julien Hanssens <j-github@hanssens.com>
2012-05-11 11:39:18 +0200

    Initial checkin of the "Handle This!" project
    - Implemented basic functionality
    - Requires refactoring and some
    special sauce styling

---
+++ Source/Hanssens.Applications.HandleThis/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace Hanssens.Applications.HandleThis
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            
+            if (e.Args != null && e.Args.Count() > 0)
+            {
+                foreach (var arg in e.Args)
+                {
+                    // Read out arguments
+                    this.Properties["Filename"] = e.Args[0];
+                }
+            }
+
+            base.OnStartup(e);
+        }
+    }
+
+}
---
+++ Source/Hanssens.Applications.HandleThis/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+using System.IO;
+
+namespace Hanssens.Applications.HandleThis
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // do this!
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (Application.Current.Properties["Filename"] != null)
+            {
+                InspectFile(Application.Current.Properties["Filename"].ToString());
+            }
+        }
+
+        protected void InspectFile(string fileName)
+        {
+            if (!File.Exists(fileName))
+                MessageBox.Show("File does not exist:\n" + fileName, "File does not exist", MessageBoxButton.OK, MessageBoxImage.Error);
+
+            // Initialize the handle.exe process
+            var tool = new Process();
+            tool.StartInfo.FileName = @".\Assets\handle.exe";
+            tool.StartInfo.Arguments = fileName;
+            tool.StartInfo.UseShellExecute = false;
+            tool.StartInfo.RedirectStandardOutput = true;
+
+            // Ensure no console window appears
+            tool.StartInfo.CreateNoWindow = true;
+            tool.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+            // Execute, and wait for results
+            tool.Start();
+            tool.WaitForExit();
+            var fullOutputMessage = tool.StandardOutput.ReadToEnd();
+
+            // Split the line endings and filter out the actual result
+            var result = fullOutputMessage
+                .Trim()         // Trim all line breaks and spaces
+                .Split('\r')    // Split by hard returns
+                .LastOrDefault()// Fetch the last argument
+                .Trim();        // Again, trim all line breaks and spaces from the result
+
+            //MessageBox.Show(result);
+            HandleResultText.Text = result;
+        }
+
+        private void InspectFileCommand_Click(object sender, RoutedEventArgs e)
+        {
+            if (!String.IsNullOrEmpty(FileToInspectText.Text))
+                InspectFile(FileToInspectText.Text);
+        }
+    }
+}
